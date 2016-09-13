365aa176e3813aad8dc98dd98cef3f699b1f8529
Nina Chaubal <nina@cupcakesystems.com>
2016-09-13 18:06:29 -0500

    Improve form accessibility

--- controllers/home.js
+++ controllers/home.js
@@ -5,18 +5,22 @@ const mcapi = require('mailchimp-api/mailchimp');
 
 /* A controller for the home page. */
 function controller(app) {
-  let mc = new mcapi.Mailchimp(process.env.MAILCHIMP_KEY);
-  let listId;
-  mc.lists.list(function(data) {
-    let lists = data.data;
-    let surveyList = lists.find(function(list) {
-      return list.name === 'Trans Mental Health Survey List'
+  let mc, listId;
+  if (process.env.MAILCHIMP_KEY) {
+    mc = new mcapi.Mailchimp(process.env.MAILCHIMP_KEY);
+    mc.lists.list(function(data) {
+      let lists = data.data;
+      let surveyList = lists.find(function(list) {
+        return list.name === 'Trans Mental Health Survey List'
+      });
+      if (!surveyList) {
+        console.log('List not found.');
+      }
+      listId = surveyList.id;
     });
-    if (!surveyList) {
-      console.log('List not found.');
-    }
-    listId = surveyList.id;
-  });
+  } else {
+    console.log('MAILCHIMP_KEY not set.');
+  }
 
   app.get('/', function (req, res) {
     let data = {
@@ -38,13 +42,17 @@ function controller(app) {
     if(req.body.last) {
       merge['LNAME'] = req.body.last;
     }
-    mc.lists.subscribe({id: listId, email:{email: email}, merge_vars: merge}, function() {
-      res.status(200).end();
-    },
-    function(err) {
-      console.log(err);
-      res.status(500).end();
-    });
+    if(mc) {
+      mc.lists.subscribe({id: listId, email:{email: email}, merge_vars: merge}, function() {
+        res.status(200).end();
+      },
+      function(err) {
+        console.log(err);
+        res.status(500).end();
+      });
+    } else {
+      console.log('To be added to list: ' + merge);
+    }
   });
 }
 
